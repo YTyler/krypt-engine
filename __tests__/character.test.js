36bb6e5dad0a43db41b2fa6a9f50e324fe8957e1
Student <example@example.com>
2019-12-02 14:27:32 -0800

    add combat math, armor aggregate

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -4,16 +4,41 @@ import {Equipment} from './../src/equipment.js'
 describe('Character', () => {
   let player = new Character(7,3,5,5);
   let longSword = new Equipment('weapon',3,-1,0,0);
-  let leatherBoots
+  let fingerGloves = new Equipment('hands',0,0,1,0);
+  let leatherBody = new Equipment('body',0,0,2,0);
+  let leatherBoots = new Equipment('legs',0,0,1,0);
+  let enemy = new Character(2,2,2,2);
+
+  player.equip(longSword);
+  player.equip(fingerGloves);
+  player.equip(leatherBody);
+  player.equip(leatherBoots);
+
+  beforeEach(() => {
+
+  });
+
   test('test stat alignment', () => {
     expect(player.stats.str).toEqual(7);
     expect(player.stats.int).toEqual(3);
     expect(player.stats.con).toEqual(5);
-    expect(player.stats.end).toEqual(5);
+    expect(player.stats.mag).toEqual(5);
   });
   test('equip function test', () => {
-    player.equip(longSword)
+
     expect(player.equipment.weapon).toEqual(longSword);
 
+    expect(player.equipment.body).toEqual(leatherBody);
   });
+  test('aggregate armor values', () => {
+    let armorTest = player.armorValues();
+    expect(armorTest[0]).toEqual(3);
+    expect(armorTest[1]).toEqual(-1);
+    expect(armorTest[2]).toEqual(3);
+    expect(armorTest[3]).toEqual(0);
+  });
+  test('combat test',() => {
+    player.attack(enemy);
+    expect(enemy.health).toEqual(10);
+  })
 })
--- src/character.js
+++ src/character.js
@@ -17,27 +17,43 @@ export class Character {
     };
     this.inventory = {bag:[], bagSpace: 10};
     this.equipment = {
-      weapon:'',
+      weapon: '',
       hands:'',
-      chest:'',
-      legs:'',
+      body:'',
       feet:'',
     };
   }
   //Character Methods
+  //Combat Methods
   attack(target) {
-    const damage = this.stats.str;
+    let mods = this.armorValues();
+    const damage = this.stats.str + mods[0];
     target.health -= damage;
     return damage;
   }
+
+  //Utility Methods
   equip(equipment) {
     const keys = Object.keys(this.equipment);
-    console.log(this.equipment);
     for (let i = 0; i < keys.length; i++) {
       if (keys[i] === equipment.type){
         this.equipment[keys[i]] = equipment;
       }
     }
+  }
+
+  armorValues() {
+    let temp = [0,0,0,0];
+    const keys = Object.keys(this.equipment);
+    for (let i = 0; i < keys.length; i++) {
+      if (this.equipment[keys[i]]) {
+        temp[0] += this.equipment[keys[i]].stats[0];
+        temp[1] += this.equipment[keys[i]].stats[1];
+        temp[2] += this.equipment[keys[i]].stats[2];
+        temp[3] += this.equipment[keys[i]].stats[3];
+      }
+    }
+    return temp;
   }
 
 }
