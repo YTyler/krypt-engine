4e19697ec936ecd1876122d07f5472dd5f42d699
Student <example@example.com>
2019-12-03 11:59:47 -0800

    add resource library

---
+++ src/deck.js
@@ -0,0 +1,40 @@
+import {Character} from './character.js';
+import {Equipment} from './equipment.js';
+import {Spell} from './spell.js';
+import {Item} from './item.js';
+
+//Enemy Quantities
+rat_num = 5;
+
+//Initialization
+
+//Characters
+let brawler = new Character();
+let mage = new Character();
+let thief = new Character();
+
+//Enemies
+for (let i = 0; i < rat_num; i++) {
+ giantRat + i = new Character;
+}
+//Items
+
+//Spells
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Itemization
